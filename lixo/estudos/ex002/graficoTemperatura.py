ab671e4692782207403e4bd93d349337cec9f379
Emanuele <esantos@gvdasa.com.br>
2021-07-07 16:48:41 -0300

    graficotemperatura

---
+++ lixo/estudos/ex002/graficoTemperatura.py
@@ -0,0 +1,21 @@
+# importar biblioteca
+from pylab import plot, show, legend, title, xlabel, ylabel
+import matplotlib.pyplot as plt
+
+# informacoes do grafico
+meses = ['Jan', 'Fev', 'Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez']
+Tmax = [38,39,32,29,26,22,19,15,20,22,25,27]
+Tmin = [23,23,22,21,19,16,12,10,16,21,22,22]
+plt.plot(meses,Tmax,meses,Tmin)
+
+# descricoes do grafico
+title('Temperaturas Maximas e Minimas do Ano')
+xlabel('Mes')
+ylabel('Temp/ºC')
+legend(['Tmax','Tmin'])
+
+# salvar imagem do grafico
+plt.savefig('ImgTemp.png',format='png', transparent = True)
+
+# exibindo o grafio
+plt.show()
